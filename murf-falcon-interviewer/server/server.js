be708afa07e6d364ed336c512ae7f4a6ac7ad4a4
Monesh <monesh1074@gmail.com>
2026-03-18 10:36:35 +0530

    clean commit

--- murf-falcon-interviewer/server/server.js
+++ murf-falcon-interviewer/server/server.js
@@ -23,7 +23,7 @@ app.use(helmet());
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'https://murfalcon-ai-ten.vercel.app/',
+    origin: process.env.CLIENT_URL || 'https://murfalcon-ai-g7od.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
