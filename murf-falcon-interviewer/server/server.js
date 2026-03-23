4023235a8383d97cd10f586c643aece19ef2cf7f
Monesh <monesh1074@gmail.com>
2026-03-23 22:16:39 +0530

    Add render deployment configuration

--- murf-falcon-interviewer/server/server.js
+++ murf-falcon-interviewer/server/server.js
@@ -25,7 +25,8 @@ app.use(
   cors({
     origin: [
     'http://localhost:5173',
-    'https://murfalcon-ai-2.onrender.com/'   // ← add this
+    'https://murfalcon-ai-2.onrender.com/',
+    process.env.CLIENT_URL
   ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
